e0d4b105ff0df42f4373757245ad9ff1d7163413
Jeanne COLMOU <jecolmou@made-f0Br5s6.clusters.42paris.fr>
2023-10-10 13:02:31 +0200

    JS openclassroom

---
+++ test/fichier.js
@@ -0,0 +1,19 @@
+let myAge = 24;
+const myAge1 = 24;
+console.log(myAge);
+
+let nbrGuest = Number("43");
+nbrGuest += Number("1");
+console.log(nbrGuest);
+
+let myCharacter =
+{
+    firstName: "Wayne",
+    name: "Bruce",
+    age: 42,
+}
+
+myCharacter.car = "Batmobile";
+
+const carCharacter = myCharacter.car;
+console.log(carCharacter);
